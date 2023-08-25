b55c5f68da384d6d7e44749360eec32293ac24cd
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-08-25 15:02:16 +0530

    Files Added

--- app.js
+++ app.js
@@ -1,61 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.type('html').send(html));
+mailchimp.setConfig({
+  apiKey: "47c3033230604785bbd88757832da00a-us8",
+  server: "us8",
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => {
+  async function run() {
+    const response = await mailchimp.ping.get();
+    console.log(response);
+  }
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+  run();
+  res.sendFile(__dirname + "/signUp.html");
+});
+app.post("/", (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  const run = async () => {
+    const response = await mailchimp.lists.addListMember("3e624827ef", {
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      },
+    });
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+    return response;
+  };
+  run().then((msg) => {
+    if (msg.status === "subscribed") {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    console.log(msg.status);
+  });
+});
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is listening on 3000");
+});
+
+// API 47c3033230604785bbd88757832da00a-us8
+//id 3e624827ef
